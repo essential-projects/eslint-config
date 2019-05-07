ce43ebe61a6982d17ac4b5e16c30e26f6f7efafa
ElRaptorus <christian.werner@5minds.de>
2019-05-07 16:52:22 +0200

    :sparkles: Add more customized rules

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ module.exports = {
     'newline-per-chained-call': ["error", { "ignoreChainWithDepth": 2 }],
     'no-new-func': ['off'],
     'operator-linebreak': ['off'],
+    'require-await': ['off'],
     '@typescript-eslint/no-explicit-any': ['warn'],
     '@typescript-eslint/no-namespace': ['off'],
     '@typescript-eslint/no-use-before-define': ['off'],
-    '@typescript-eslint/no-unused-vars': ['warn']
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    '@typescript-eslint/prefer-interface': ['off']
   }
 }
