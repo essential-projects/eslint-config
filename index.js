29021b5f1f3e15f747ba7af9eb5b236e2dc1fd39
ElRaptorus <christian.werner@5minds.de>
2019-11-28 08:46:23 +0100

    :no_entry_sign: Disable no-null and eqeqeq

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ module.exports = {
   'extends': require.resolve('eslint-config-5minds-typescript/fast'),
   'rules': {
     'arrow-body-style': ['off'],
+    'eqeqeq': ['off'],
     'import/no-extraneous-dependencies': ['off'],
     'newline-per-chained-call': ['error', { 'ignoreChainWithDepth': 2 }],
     'no-case-declarations': ['off'],
     'no-new-func': ['off'],
+    'no-null/no-null': ['off'],
     'no-param-reassign': ['off'],
     'no-plusplus': ['off'],
     'no-use-before-define': ['off'],
